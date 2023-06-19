c289fd530b655065f44113d1a243b21db14c2840
Антон Швец <a.shvec@santehnika-tut.ru>
2023-06-19 15:16:50 +0300

    Добавлен базовый функционал

--- src/App.js
+++ src/App.js
@@ -1,24 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [value, setValue] = useState(5);
+  const [options, setOptions] = useState({
+    upperCase: true,
+    lowerCase: true,
+    includeNumbers: true,
+    includeSymbols: true
+  })
+
+  function makeid(length) {
+    let result = "";
+    let characters = "";
+    const charactersLower = "abcdefghijklmnopqrstuvwxyz";
+    const charactersUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const characterNumbers = "0123456789";
+    const characterSymbols = "!@#$%^&*()-_+=;:,./?|`~[]{}"
+
+    if(options.lowerCase) {
+      characters = characters.concat(charactersLower)
+    }
+    if(options.upperCase) {
+      characters = characters.concat(charactersUpper)
+    }
+    if(options.includeNumbers) {
+      characters = characters.concat(characterNumbers)
+    }
+    if(options.includeSymbols) {
+      characters = characters.concat(characterSymbols)
+    }
+
+    const charactersLength = characters.length;
+    let counter = 0;
+    while (counter < length) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+      counter += 1;
+    }
+
+
+    return result;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <div>{options.upperCase}</div>
+      <div>{makeid(value)}</div>
+
+      <div>
+        <input
+          type="range"
+          id="character"
+          min="0"
+          max="20"
+          value={value}
+          onChange={(e) => {
+            setValue(e.target.value);
+          }}
+        />
+        <label htmlFor="character">{value}</label>
+      </div>
+    </>
   );
 }
 
