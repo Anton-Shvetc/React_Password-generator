0c6e86ab064e7ae463d373b5a833a853b4d42065
Антон Швец <a.shvec@santehnika-tut.ru>
2023-06-19 16:23:19 +0300

    Добавлен функционал вывода уровня сложности

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
   const [value, setValue] = useState(5);
   const [options, setOptions] = useState({
     upperCase: true,
-    lowerCase: true,
-    includeNumbers: true,
-    includeSymbols: true
-  })
+    lowerCase: false,
+    includeNumbers: false,
+    includeSymbols: false,
+  });
+  const [levelValue, setLevelValue] = useState(0);
 
   function makeid(length) {
     let result = "";
@@ -15,19 +16,19 @@ function App() {
     const charactersLower = "abcdefghijklmnopqrstuvwxyz";
     const charactersUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     const characterNumbers = "0123456789";
-    const characterSymbols = "!@#$%^&*()-_+=;:,./?|`~[]{}"
+    const characterSymbols = "!@#$%^&*()-_+=;:,./?|`~[]{}";
 
-    if(options.lowerCase) {
-      characters = characters.concat(charactersLower)
+    if (options.lowerCase) {
+      characters = characters.concat(charactersLower);
     }
-    if(options.upperCase) {
-      characters = characters.concat(charactersUpper)
+    if (options.upperCase) {
+      characters = characters.concat(charactersUpper);
     }
-    if(options.includeNumbers) {
-      characters = characters.concat(characterNumbers)
+    if (options.includeNumbers) {
+      characters = characters.concat(characterNumbers);
     }
-    if(options.includeSymbols) {
-      characters = characters.concat(characterSymbols)
+    if (options.includeSymbols) {
+      characters = characters.concat(characterSymbols);
     }
 
     const charactersLength = characters.length;
@@ -37,13 +38,36 @@ function App() {
       counter += 1;
     }
 
-
     return result;
+  }
+
+  useEffect(() => {
+    let count = 0;
+    for (let key in options) {
+      if (options[key] === true) {
+        count++;
+      }
+    }
+
+    setLevelValue(count);
+  }, [options]);
+
+  function renderSwitch() {
+    switch (levelValue) {
+      case 1 || 2:
+        return "easy";
+      case 3:
+        return "medium";
+      case 4:
+        return "hard";
+      default:
+        return "easy";
+    }
   }
 
   return (
     <>
-    <div>{options.upperCase}</div>
+      <div>{renderSwitch()}</div>
       <div>{makeid(value)}</div>
 
       <div>
